43fd6889f901768bc49f77888ed960e329d9704a
emirontop1 <dincemr@gmail.com>
2026-07-08 21:01:20 +0300

    Create obfuscator.js

---
+++ obfuscator.js
@@ -0,0 +1,26 @@
+function obfuscateString(str) {
+    // Yazıları bilgisayarın anlayacağı \x48\x65 formatına çevirir
+    return str.replace(/"([^"\\]*)"/g, function(match, p1) {
+        let hexed = "";
+        for (let i = 0; i < p1.length; i++) {
+            hexed += "\\" + p1.charCodeAt(i).toString(16);
+        }
+        return '"' + hexed + '"';
+    });
+}
+
+function startObfuscation() {
+    let input = document.getElementById("inputCode").value;
+    
+    // 1. Aşama: Yazıları şifrele (Örn: "Merhaba" -> "\x4d\x65...")
+    let result = obfuscateString(input);
+    
+    // 2. Aşama: Basitçe değişken isimlerini değiştir (Örn: 'mesaj' -> '_0xX1')
+    result = result.replace(/\bmesaj\b/g, "_0xX1");
+    
+    // 3. Aşama: Başına bir filigran/imza ekle
+    let finalCode = `-- Obfuscated by MyGitHubObfuscator\n${result}`;
+    
+    document.getElementById("outputCode").value = finalCode;
+}
+
